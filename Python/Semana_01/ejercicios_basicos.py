37f8a692b499dd8b8d475910a88a36bf8e10f8e5
areliarispe <168610292+arisper07@users.noreply.github.com>
2026-06-15 11:49:44 -0500

    Agregar ejercicio de máximo y mínimo en una tupla

--- Python/Semana_01/ejercicios_basicos.py
+++ Python/Semana_01/ejercicios_basicos.py
@@ -1,1 +1,6 @@
-
+## CREAR UNA TUPLA DE NROS, ENCUENTRA EL NRO MAS GRANDE Y EL MAS PEQUEÑO
+numeros = (15,7,42,3,25,9)
+maximo = max(numeros)
+minimo = min(numeros)
+print('el maximo valor es: ', maximo)
+print('el maximo valor es: ', minimo)
